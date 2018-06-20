c10845e309dc2c0018c0fccc565ce4cd9d929b1e
hwholiday <heweiholiday@gmail.com>
2018-06-20 20:05:36 +0800

    context study

---
+++ context/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var siganChannel = make(chan os.Signal, 1)
+
+func main() {
+	//ContextWithCancel()
+	//ContextWithTimeout()
+	ContextWithDeadline()
+}
+
+func ContextWithCancel() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println(ctx.Value("howie"))
+				return
+			}
+		}
+	}()
+	fmt.Println("开始")
+	time.AfterFunc(10*time.Second, func() {
+		ctx = context.WithValue(ctx, "howie", "10秒后调用cancel()")
+		cancel()
+		fmt.Println("结束")
+	})
+	Exit()
+
+}
+
+func ContextWithTimeout() {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+	start := time.Now()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println(time.Since(start))
+				fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+				return
+			}
+		}
+	}()
+	Exit()
+
+}
+
+func ContextWithDeadline() {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+	start := time.Now()
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(10*time.Second))
+	defer cancel()
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println(time.Since(start))
+				fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+				return
+			}
+		}
+	}()
+	Exit()
+
+}
+
+func Exit() {
+	signal.Notify(siganChannel, os.Kill, os.Interrupt)
+	<-siganChannel
+}
