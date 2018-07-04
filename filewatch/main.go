5f79e666fcf804db84f54dd6fe48bf906a8dfc2c
hwholiday <heweiholiday@gmail.com>
2018-07-04 14:41:32 +0800

    filewatch study

---
+++ filewatch/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"path/filepath"
+	"os"
+	"fmt"
+	"log"
+)
+
+func main()  {
+	watch, _ := fsnotify.NewWatcher()
+	w := Watch{
+		watch: watch,
+	}
+	w.watchDir("./filewatch/watch")
+	select {}
+}
+
+type Watch struct {
+	watch *fsnotify.Watcher
+}
+
+func (w *Watch) watchDir(dir string) {
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			path, err := filepath.Abs(path)
+			if err != nil {
+				return err
+			}
+			err = w.watch.Add(path)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	log.Println("监控服务已经启动")
+	go func() {
+		for {
+			select {
+			case ev := <-w.watch.Events:
+				{
+					if ev.Op&fsnotify.Create == fsnotify.Create {
+						fmt.Println("创建文件 : ", ev.Name)
+						fi, err := os.Stat(ev.Name);
+						if err == nil && fi.IsDir() {
+							w.watch.Add(ev.Name);
+							fmt.Println("添加监控 : ", ev.Name)
+						}
+					}
+					if ev.Op&fsnotify.Write == fsnotify.Write {
+						fmt.Println("写入文件 : ", ev.Name)
+					}
+					if ev.Op&fsnotify.Remove == fsnotify.Remove {
+						fmt.Println("删除文件 : ", ev.Name)
+						fi, err := os.Stat(ev.Name)
+						if err == nil && fi.IsDir() {
+							w.watch.Remove(ev.Name)
+							fmt.Println("删除监控 : ", ev.Name)
+						}
+					}
+					if ev.Op&fsnotify.Rename == fsnotify.Rename {
+						fmt.Println("重命名文件 : ", ev.Name)
+						w.watch.Remove(ev.Name)
+					}
+				}
+			case err := <-w.watch.Errors:
+				{
+					fmt.Println("error : ", err)
+					return
+				}
+			}
+		}
+	}()
+}
