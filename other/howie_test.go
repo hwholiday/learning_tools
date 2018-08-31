1c8e440d5b838303b703979baa0b48ed60640444
hwholiday <ai19950306>
2018-08-31 13:18:50 +0800

    统一检查err

---
+++ other/howie_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+	"github.com/kataras/go-errors"
+	"fmt"
+)
+
+func Test(t *testing.T) {
+	checkErr := func(err error) {
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	}
+	checkErr(Get())
+	checkErr(GetNil())
+}
+
+func Get() error {
+	return errors.New("errr")
+}
+
+func GetNil() error {
+	return nil
+}
