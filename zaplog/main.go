e949d959d8efad4d6004f866289c34967cd21c25
hwholiday <ai19950306>
2018-09-19 17:01:43 +0800

    使用zap log日志库

---
+++ zaplog/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"time"
+	"github.com/kataras/go-errors"
+)
+
+var Logger *zap.Logger
+
+func InitLogger() {
+	// 日志地址 "out.log" 自定义
+	lp := "out.log"
+	// 日志级别 DEBUG,ERROR, INFO
+	lv := "DEBUG"
+	isDebug := false
+
+	initLogger(lp, lv, isDebug)
+	log.SetFlags(log.Lmicroseconds | log.Lshortfile | log.LstdFlags)
+}
+func TimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString("[" + t.Format("2006-01-02 15:04:05") + "]")
+}
+
+func initLogger(lp string, lv string, isDebug bool) {
+	var js string
+	if isDebug {
+		js = fmt.Sprintf(`{
+      "level": "%s",
+      "encoding": "console",
+      "outputPaths": ["stdout"],
+      "errorOutputPaths": ["stdout"]
+      }`, lv)
+	} else {
+		js = fmt.Sprintf(`{
+      "level": "%s",
+      "encoding": "json",
+      "outputPaths": ["%s"],
+      "errorOutputPaths": ["%s"]
+      }`, lv, lp, lp)
+	}
+	var cfg zap.Config
+	if err := json.Unmarshal([]byte(js), &cfg); err != nil {
+		panic(err)
+	}
+	cfg.EncoderConfig = zap.NewProductionEncoderConfig()
+	cfg.EncoderConfig.EncodeTime = TimeEncoder
+	var err error
+	Logger, err = cfg.Build()
+	if err != nil {
+		log.Fatal("init logger error: ", err)
+	}
+}
+func main() {
+	InitLogger()
+	s := []string{
+		"hello info",
+		"hello error",
+		"hello debug",
+		"hello fatal",
+	}
+	Logger.Info("info:", zap.Error(errors.New("123123")))
+	Logger.Error("info:", zap.String("s", s[1]))
+	Logger.Debug("info:", zap.String("s", s[2]))
+	Logger.Fatal("info:", zap.String("s", s[3]))
+}
