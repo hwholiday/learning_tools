2545a8447150c184a988061dfd69d658abd8c592
hwholiday <heweiholiday@gmail.com>
2019-01-28 13:48:47 +0800

    cli 使用

---
+++ cli/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "howie"
+	app.Version = "1.0.0"
+	app.Email = "520@520.com"
+	app.Commands = []cli.Command{
+		{
+			Name:    "prot",
+			Aliases: []string{"p"},
+			Usage:   "用户端口",
+			Before: func(c *cli.Context) error {
+				fmt.Println("Before")
+				return nil
+			},
+			Action: func(c *cli.Context) {
+				fmt.Println("Action")
+				fmt.Println(c.Args().First())
+			},
+			After: func(c *cli.Context) error {
+				fmt.Println("After")
+				return nil
+			},
+		},
+		{
+			Name:    "isdebug",
+			Aliases: []string{"p"},
+			Usage:   "is debug",
+			Before: func(c *cli.Context) error {
+				fmt.Println("Before")
+				return nil
+			},
+			Action: func(c *cli.Context) {
+				fmt.Println("Action")
+				fmt.Println(c.Args().First())
+			},
+			After: func(c *cli.Context) error {
+				fmt.Println("After")
+				return nil
+			},
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
+}
