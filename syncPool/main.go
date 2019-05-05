30bcb4c2f06c050f8c2a5cc7038c17b0624fe989
hwholiday <heweiholiday@gmail.com>
2019-05-05 15:32:25 +0800

    使用sync.pool包

---
+++ syncPool/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"sync"
+)
+
+func main() {
+	// 建立对象
+	var pipe = &sync.Pool{New:func()interface{}{return "Hello"}}
+	// 准备放入的字符串
+	val := "Hello,World!"
+	// 放入
+	pipe.Put(val)
+	// 取出
+	log.Println(pipe.Get())
+	// 再取就没有了,会自动调用NEW
+	log.Println(pipe.Get())
+
+}
