c3b8f89c5367389b5fbf1a31f591137d57ad44dd
hwholiday <heweiholiday@gmail.com>
2019-05-13 16:21:19 +0800

    修改zaplog使用

---
+++ zaplog/log_test.go
@@ -0,0 +1,18 @@
+package zaplog
+
+import (
+	"testing"
+	"go.uber.org/zap"
+	"fmt"
+)
+
+func TestInitZapV2Logger(t *testing.T) {
+	InitZapLogger(&ToolLogger{Filename: "logtool.log", MaxSize: 3, MaxAge: 30, MaxBackups: 30, Compress: false, Level:zap.InfoLevel})
+	for i := 0; i < 10000; i++ {
+		Zap.Info(fmt.Sprint("test log ", i), zap.Int("line", 47))
+		Zap.Debug(fmt.Sprint("debug log ", i), zap.Any("level", "1231231231"))
+		Zap.Info(fmt.Sprint("Info log ", i), zap.String("level", `{"a":"4","b":"5"}`))
+		Zap.Warn(fmt.Sprint("Info log ", i), zap.String("level", `{"a":"7","b":"8"}`))
+	}
+}
+
