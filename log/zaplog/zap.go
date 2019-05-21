a657b7c157c25acb8dbe432bd336a8fc56cb9bee
hwholiday <heweiholiday@gmail.com>
2019-05-21 20:59:00 +0800

    日志分等级存放,大小切分

---
+++ log/zaplog/zap.go
@@ -0,0 +1,195 @@
+package zaplog
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+// Options 配置项
+type Options struct {
+	zap.Config
+	LogFileDir    string `json:logFileDir`
+	AppName       string `json:"appName"`
+	ErrorFileName string `json:"errorFileName"`
+	WarnFileName  string `json:"warnFileName"`
+	InfoFileName  string `json:"infoFileName"`
+	DebugFileName string `json:"debugFileName"`
+	MaxSize       int    `json:"maxSize"` // megabytes
+	MaxBackups    int    `json:"maxBackups"`
+	MaxAge        int    `json:"maxAge"` // days
+}
+
+var (
+	l                              *Logger
+	sp                             = string(filepath.Separator)
+	errWS, warnWS, infoWS, debugWS zapcore.WriteSyncer       // IO输出
+	debugConsoleWS                 = zapcore.Lock(os.Stdout) // 控制台标准输出
+	errorConsoleWS                 = zapcore.Lock(os.Stderr)
+)
+
+func init() {
+	l = &Logger{
+		Opts: &Options{},
+	}
+}
+
+type Logger struct {
+	*zap.Logger
+	sync.RWMutex
+	Opts      *Options `json:"opts"`
+	zapConfig zap.Config
+	inited    bool
+}
+
+func InitLogger() {
+	l.Lock()
+	defer l.Unlock()
+	if l.inited {
+		l.Info("[initLogger] logger Inited")
+		return
+	}
+	l.loadCfg()
+	l.init()
+	l.Info("[initLogger] zap plugin initializing completed")
+	l.inited = true
+}
+
+// GetLogger returns logger
+func GetLogger() (ret *Logger) {
+	return l
+}
+
+func (l *Logger) init() {
+
+	l.setSyncers()
+	var err error
+	l.Logger, err = l.zapConfig.Build(l.cores())
+	if err != nil {
+		panic(err)
+	}
+	defer l.Logger.Sync()
+}
+
+func (l *Logger) loadCfg() {
+	//TODO 把配置文件读入log类
+	if l.Opts.Development {
+		l.zapConfig = zap.NewDevelopmentConfig()
+	} else {
+		l.zapConfig = zap.NewProductionConfig()
+	}
+	// application log output path
+	if l.Opts.OutputPaths == nil || len(l.Opts.OutputPaths) == 0 {
+		l.zapConfig.OutputPaths = []string{"stdout"}
+	}
+	//  error of zap-self log
+	if l.Opts.ErrorOutputPaths == nil || len(l.Opts.ErrorOutputPaths) == 0 {
+		l.zapConfig.OutputPaths = []string{"stderr"}
+	}
+	// 默认输出到程序运行目录的logs子目录
+	if l.Opts.LogFileDir == "" {
+		l.Opts.LogFileDir, _ = filepath.Abs(filepath.Dir(filepath.Join(".")))
+		l.Opts.LogFileDir += sp + "logs" + sp
+	}
+
+	if l.Opts.AppName == "" {
+		l.Opts.AppName = "app"
+	}
+
+	if l.Opts.ErrorFileName == "" {
+		l.Opts.ErrorFileName = "error.log"
+	}
+
+	if l.Opts.WarnFileName == "" {
+		l.Opts.WarnFileName = "warn.log"
+	}
+
+	if l.Opts.InfoFileName == "" {
+		l.Opts.InfoFileName = "info.log"
+	}
+
+	if l.Opts.DebugFileName == "" {
+		l.Opts.DebugFileName = "debug.log"
+	}
+
+	if l.Opts.MaxSize == 0 {
+		l.Opts.MaxSize = 50
+	}
+	if l.Opts.MaxBackups == 0 {
+		l.Opts.MaxBackups = 3
+	}
+	if l.Opts.MaxAge == 0 {
+		l.Opts.MaxAge = 30
+	}
+}
+
+func (l *Logger) setSyncers() {
+	f := func(fN string) zapcore.WriteSyncer {
+		return zapcore.AddSync(&lumberjack.Logger{
+			Filename:   l.Opts.LogFileDir + sp + l.Opts.AppName + "-" + fN,
+			MaxSize:    l.Opts.MaxSize,
+			MaxBackups: l.Opts.MaxBackups,
+			MaxAge:     l.Opts.MaxAge,
+			Compress:   true,
+			LocalTime:  true,
+		})
+	}
+	errWS = f(l.Opts.ErrorFileName)
+	warnWS = f(l.Opts.WarnFileName)
+	infoWS = f(l.Opts.InfoFileName)
+	debugWS = f(l.Opts.DebugFileName)
+	return
+}
+
+func (l *Logger) cores() zap.Option {
+	fileEncoder := zapcore.NewJSONEncoder(l.zapConfig.EncoderConfig)
+	consoleEncoder := zapcore.NewConsoleEncoder(l.zapConfig.EncoderConfig)
+	errPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl > zapcore.WarnLevel && zapcore.WarnLevel-l.zapConfig.Level.Level() > -1
+	})
+	warnPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl == zapcore.WarnLevel && zapcore.WarnLevel-l.zapConfig.Level.Level() > -1
+	})
+	infoPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl == zapcore.InfoLevel && zapcore.InfoLevel-l.zapConfig.Level.Level() > -1
+	})
+	debugPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl == zapcore.DebugLevel && zapcore.DebugLevel-l.zapConfig.Level.Level() > -1
+	})
+	cores := []zapcore.Core{
+		// region 日志文件
+		// error 及以上
+		zapcore.NewCore(fileEncoder, errWS, errPriority),
+		// warn
+		zapcore.NewCore(fileEncoder, warnWS, warnPriority),
+		// info
+		zapcore.NewCore(fileEncoder, infoWS, infoPriority),
+		// debug
+		zapcore.NewCore(fileEncoder, debugWS, debugPriority),
+		// endregion
+		// region 控制台
+		// 错误及以上
+		zapcore.NewCore(consoleEncoder, errorConsoleWS, errPriority),
+		// 警告
+		zapcore.NewCore(consoleEncoder, debugConsoleWS, warnPriority),
+		// info
+		zapcore.NewCore(consoleEncoder, debugConsoleWS, infoPriority),
+		// debug
+		zapcore.NewCore(consoleEncoder, debugConsoleWS, debugPriority),
+		// endregion
+	}
+	return zap.WrapCore(func(c zapcore.Core) zapcore.Core {
+		return zapcore.NewTee(cores...)
+	})
+}
+func timeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString("[" + t.Format("2006-01-02 15:04:05") + "]")
+}
+
+func timeUnixNano(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendInt64(t.UnixNano())
+}
