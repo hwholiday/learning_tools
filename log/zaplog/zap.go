3d1a588f3e13f5c9d7d308ffdbe76f0dc776ef89
hwholiday <heweiholiday@gmail.com>
2019-05-22 13:51:37 +0800

    zap 优化

--- log/zaplog/zap.go
+++ log/zaplog/zap.go
@@ -10,18 +10,17 @@ import (
 	"time"
 )
 
-// Options 配置项
 type Options struct {
+	LogFileDir    string
+	AppName       string
+	ErrorFileName string
+	WarnFileName  string
+	InfoFileName  string
+	DebugFileName string
+	MaxSize       int
+	MaxBackups    int
+	MaxAge        int
 	zap.Config
-	LogFileDir    string `json:logFileDir`
-	AppName       string `json:"appName"`
-	ErrorFileName string `json:"errorFileName"`
-	WarnFileName  string `json:"warnFileName"`
-	InfoFileName  string `json:"infoFileName"`
-	DebugFileName string `json:"debugFileName"`
-	MaxSize       int    `json:"maxSize"` // megabytes
-	MaxBackups    int    `json:"maxBackups"`
-	MaxAge        int    `json:"maxAge"` // days
 }
 
 var (
@@ -46,12 +45,15 @@ type Logger struct {
 	inited    bool
 }
 
-func InitLogger() {
+func initLogger(cf ...*Options) {
 	l.Lock()
 	defer l.Unlock()
 	if l.inited {
 		l.Info("[initLogger] logger Inited")
 		return
+	}
+	if len(cf) > 0 {
+		l.Opts = cf[0]
 	}
 	l.loadCfg()
 	l.init()
@@ -65,7 +67,6 @@ func GetLogger() (ret *Logger) {
 }
 
 func (l *Logger) init() {
-
 	l.setSyncers()
 	var err error
 	l.Logger, err = l.zapConfig.Build(l.cores())
@@ -76,7 +77,6 @@ func (l *Logger) init() {
 }
 
 func (l *Logger) loadCfg() {
-	//TODO 把配置文件读入log类
 	if l.Opts.Development {
 		l.zapConfig = zap.NewDevelopmentConfig()
 		l.zapConfig.EncoderConfig.EncodeTime = timeEncoder
@@ -84,11 +84,9 @@ func (l *Logger) loadCfg() {
 		l.zapConfig = zap.NewProductionConfig()
 		l.zapConfig.EncoderConfig.EncodeTime = timeUnixNano
 	}
-	// application log output path
 	if l.Opts.OutputPaths == nil || len(l.Opts.OutputPaths) == 0 {
 		l.zapConfig.OutputPaths = []string{"stdout"}
 	}
-	//  error of zap-self log
 	if l.Opts.ErrorOutputPaths == nil || len(l.Opts.ErrorOutputPaths) == 0 {
 		l.zapConfig.OutputPaths = []string{"stderr"}
 	}
@@ -97,27 +95,21 @@ func (l *Logger) loadCfg() {
 		l.Opts.LogFileDir, _ = filepath.Abs(filepath.Dir(filepath.Join(".")))
 		l.Opts.LogFileDir += sp + "logs" + sp
 	}
-
 	if l.Opts.AppName == "" {
 		l.Opts.AppName = "app"
 	}
-
 	if l.Opts.ErrorFileName == "" {
 		l.Opts.ErrorFileName = "error.log"
 	}
-
 	if l.Opts.WarnFileName == "" {
 		l.Opts.WarnFileName = "warn.log"
 	}
-
 	if l.Opts.InfoFileName == "" {
 		l.Opts.InfoFileName = "info.log"
 	}
-
 	if l.Opts.DebugFileName == "" {
 		l.Opts.DebugFileName = "debug.log"
 	}
-
 	if l.Opts.MaxSize == 0 {
 		l.Opts.MaxSize = 50
 	}
@@ -149,7 +141,11 @@ func (l *Logger) setSyncers() {
 
 func (l *Logger) cores() zap.Option {
 	fileEncoder := zapcore.NewJSONEncoder(l.zapConfig.EncoderConfig)
-	consoleEncoder := zapcore.NewConsoleEncoder(l.zapConfig.EncoderConfig)
+	//consoleEncoder := zapcore.NewConsoleEncoder(l.zapConfig.EncoderConfig)
+	encoderConfig := zap.NewDevelopmentEncoderConfig()
+	encoderConfig.EncodeTime = timeEncoder
+	consoleEncoder := zapcore.NewConsoleEncoder(encoderConfig)
+
 	errPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
 		return lvl > zapcore.WarnLevel && zapcore.WarnLevel-l.zapConfig.Level.Level() > -1
 	})
@@ -163,26 +159,18 @@ func (l *Logger) cores() zap.Option {
 		return lvl == zapcore.DebugLevel && zapcore.DebugLevel-l.zapConfig.Level.Level() > -1
 	})
 	cores := []zapcore.Core{
-		// region 日志文件
-		// error 及以上
 		zapcore.NewCore(fileEncoder, errWS, errPriority),
-		// warn
 		zapcore.NewCore(fileEncoder, warnWS, warnPriority),
-		// info
 		zapcore.NewCore(fileEncoder, infoWS, infoPriority),
-		// debug
 		zapcore.NewCore(fileEncoder, debugWS, debugPriority),
-		// endregion
-		// region 控制台
-		// 错误及以上
-		zapcore.NewCore(consoleEncoder, errorConsoleWS, errPriority),
-		// 警告
-		zapcore.NewCore(consoleEncoder, debugConsoleWS, warnPriority),
-		// info
-		zapcore.NewCore(consoleEncoder, debugConsoleWS, infoPriority),
-		// debug
-		zapcore.NewCore(consoleEncoder, debugConsoleWS, debugPriority),
-		// endregion
+	}
+	if l.Opts.Development {
+		cores = append(cores, []zapcore.Core{
+			zapcore.NewCore(consoleEncoder, errorConsoleWS, errPriority),
+			zapcore.NewCore(consoleEncoder, debugConsoleWS, warnPriority),
+			zapcore.NewCore(consoleEncoder, debugConsoleWS, infoPriority),
+			zapcore.NewCore(consoleEncoder, debugConsoleWS, debugPriority),
+		}...)
 	}
 	return zap.WrapCore(func(c zapcore.Core) zapcore.Core {
 		return zapcore.NewTee(cores...)
@@ -193,5 +181,5 @@ func timeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 }
 
 func timeUnixNano(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-	enc.AppendInt64(t.UnixNano())
+	enc.AppendInt64(t.UnixNano() / 1e6)
 }
