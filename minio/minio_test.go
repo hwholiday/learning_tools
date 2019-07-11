f4df4c74fddc09577adc57c5706ee864590fe58d
hwholiday <heweiholiday@gmail.com>
2019-07-11 18:26:36 +0800

    合并文件操作

--- minio/minio_test.go
+++ minio/minio_test.go
@@ -12,6 +12,8 @@ var minioClient *minio.Client
 
 func init() {
 	var err error
+	//s3.amazonaws.com
+	//127.0.0.1:9000
 	minioClient, err = minio.New("127.0.0.1:9000", "AKIAINCHU2DIYAQ66TPA", "ZaYtz0d61fJXQ7djyXBX4yZ5ob8Kj/WNXtw6PJob", false)
 	if err != nil {
 		panic(err)
@@ -34,6 +36,49 @@ func TestGetObject(t *testing.T) {
 	}
 	fmt.Println(string(data))
 }
+func TestPutObject(t *testing.T)  {
+	/*_,err:=minioClient.PutObject("test","3",strings.NewReader("3"),int64(len("1")),minio.PutObjectOptions{})
+	if err != nil {
+		panic(err)
+	}*/
+	info,err:=minioClient.StatObject("test","1",minio.StatObjectOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(info)
+	fmt.Println(info.ETag)
+}
+
+
+func TestComposeObject(t *testing.T)  {
+    bucketName:="test"
+	var srcs =make([]minio.SourceInfo,0)
+	for i:=0;i<3;i++{
+		name:=fmt.Sprintf("sheet_%d",i)
+		bbuf := bytes.Repeat([]byte(fmt.Sprint(i+1)), 1024*1025*5)//至少要5M
+
+		index,err:=minioClient.PutObject(bucketName,name,bytes.NewReader(bbuf),int64(len(bbuf)),minio.PutObjectOptions{})
+        if err!=nil{
+       	   panic(err)
+	    }
+		fmt.Println(index)
+		srcs=append(srcs,minio.NewSourceInfo(bucketName,fmt.Sprintf("sheet_%d",i),nil))
+	}
+	fmt.Println(srcs)
+	dst, err := minio.NewDestinationInfo("test", "222.log", nil, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(dst)
+	err = minioClient.ComposeObject(dst, srcs)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Composed object successfully.")
+}
+
 
 func TestNewMultipartUpload(t *testing.T) {
 	daya := minio.Core{Client: minioClient}
@@ -46,7 +91,8 @@ func TestNewMultipartUpload(t *testing.T) {
 }
 
 func TestNewPutObjectPart(t *testing.T) {
-	abuf := bytes.Repeat([]byte("aaaaa"), 1024*1024)//至少要5MB 才能使用分片上传
+	abuf := bytes.Repeat([]byte("aaaaa"), 1024*1024)//至少要5MB 才能使用分片上传(不包含最后一片)
+	fmt.Println(len(abuf))
 	bbuf := bytes.Repeat([]byte("b"), 1024*1024)
 	daya := minio.Core{Client: minioClient}
 	bucket:="test"
@@ -75,5 +121,20 @@ func TestNewPutObjectPart(t *testing.T) {
 	_, err = daya.CompleteMultipartUpload(bucket, name, v1, completeParts)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func TestConsolidatedFile(t *testing.T)  {
+	bucketName:="test"
+	var srcs =make([]minio.SourceInfo,0)
+	for i:=0;i<3;i++{
+		name:=fmt.Sprintf("sheet_%d",i)
+		bbuf := bytes.Repeat([]byte(fmt.Sprint(i+1)), 1)//至少要5M
+		index,err:=minioClient.PutObject(bucketName,name,bytes.NewReader(bbuf),int64(len(bbuf)),minio.PutObjectOptions{})
+		if err!=nil{
+			panic(err)
+		}
+		fmt.Println(index)
+		srcs=append(srcs,minio.NewSourceInfo(bucketName,fmt.Sprintf("sheet_%d",i),nil))
 	}
 }
