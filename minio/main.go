8fdc0c9b599585a5c2f6aae04dc441c0e617cbdc
hwholiday <heweiholiday@gmail.com>
2019-07-12 10:41:58 +0800

    修改minio基础使用

--- minio/main.go
+++ minio/main.go
@@ -9,42 +9,7 @@ import (
 )
 
 func main() {
-	var (
-		bucket = "bat-app"
-	)
-	minioClient, err := minio.New("192.168.2.28:9090", "672I9BRC71NVDCMASYQQ", "yl1AGr-ViO6queLv93EFlRv-iZ1icSvUVE8j6pi9", false)
-	CheckErr("minio.New", err)
-	if err = minioClient.MakeBucket(bucket, ""); err != nil {
-		CheckErr("创建储存桶", err)
-	}
-	fmt.Println("创建桶成功:" + bucket)
-	//设置桶的权限
-	policy := `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"AWS":["*"]},"Action":["s3:ListBucketMultipartUploads","s3:GetBucketLocation","s3:ListBucket"],"Resource":["arn:aws:s3:::abc-test-1"]},{"Effect":"Allow","Principal":{"AWS":["*"]},"Action":["s3:ListMultipartUploadParts","s3:PutObject","s3:AbortMultipartUpload","s3:DeleteObject","s3:GetObject"],"Resource":["arn:aws:s3:::abc-test-1/*"]}]}`
-	err = minioClient.SetBucketPolicy(bucket, policy)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 
-	//判断文件是否存在
-	fmt.Println(minioClient.StatObject(bucket, "conf.txt", minio.StatObjectOptions{}))
-
-	//生成上传链接
-	expiry := time.Second * 24 * 60 * 60 // 1 day.
-	presignedURL, err := minioClient.PresignedPutObject(bucket, "PayDemo.java", expiry)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("Successfully generated presigned URL", presignedURL)
-	reqParams := make(url.Values)
-	reqParams.Set("Content-Type", "image/jpeg")
-	presignedURL, err = minioClient.Presign("PUT", bucket, "20190306121434.jpg", time.Second*24*60*60, reqParams)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("Successfully generated presigned URL \n", presignedURL)
 
 }
 
--- minio/minio_test.go
+++ minio/minio_test.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/minio/minio-go"
 	"io/ioutil"
+	"net/url"
 	"testing"
+	"time"
 )
 
 var minioClient *minio.Client
@@ -36,12 +38,12 @@ func TestGetObject(t *testing.T) {
 	}
 	fmt.Println(string(data))
 }
-func TestPutObject(t *testing.T)  {
+func TestPutObject(t *testing.T) {
 	/*_,err:=minioClient.PutObject("test","3",strings.NewReader("3"),int64(len("1")),minio.PutObjectOptions{})
 	if err != nil {
 		panic(err)
 	}*/
-	info,err:=minioClient.StatObject("test","1",minio.StatObjectOptions{})
+	info, err := minioClient.StatObject("test", "1", minio.StatObjectOptions{})
 	if err != nil {
 		panic(err)
 	}
@@ -49,20 +51,19 @@ func TestPutObject(t *testing.T)  {
 	fmt.Println(info.ETag)
 }
 
+func TestComposeObject(t *testing.T) {
+	bucketName := "test"
+	var srcs = make([]minio.SourceInfo, 0)
+	for i := 0; i < 3; i++ {
+		name := fmt.Sprintf("sheet_%d", i)
+		bbuf := bytes.Repeat([]byte(fmt.Sprint(i+1)), 1024*1025*5) //至少要5M
 
-func TestComposeObject(t *testing.T)  {
-    bucketName:="test"
-	var srcs =make([]minio.SourceInfo,0)
-	for i:=0;i<3;i++{
-		name:=fmt.Sprintf("sheet_%d",i)
-		bbuf := bytes.Repeat([]byte(fmt.Sprint(i+1)), 1024*1025*5)//至少要5M
-
-		index,err:=minioClient.PutObject(bucketName,name,bytes.NewReader(bbuf),int64(len(bbuf)),minio.PutObjectOptions{})
-        if err!=nil{
-       	   panic(err)
-	    }
+		index, err := minioClient.PutObject(bucketName, name, bytes.NewReader(bbuf), int64(len(bbuf)), minio.PutObjectOptions{})
+		if err != nil {
+			panic(err)
+		}
 		fmt.Println(index)
-		srcs=append(srcs,minio.NewSourceInfo(bucketName,fmt.Sprintf("sheet_%d",i),nil))
+		srcs = append(srcs, minio.NewSourceInfo(bucketName, fmt.Sprintf("sheet_%d", i), nil))
 	}
 	fmt.Println(srcs)
 	dst, err := minio.NewDestinationInfo("test", "222.log", nil, nil)
@@ -79,7 +80,6 @@ func TestComposeObject(t *testing.T)  {
 	fmt.Println("Composed object successfully.")
 }
 
-
 func TestNewMultipartUpload(t *testing.T) {
 	daya := minio.Core{Client: minioClient}
 	v1, err := daya.NewMultipartUpload("test", "2.log", minio.PutObjectOptions{})
@@ -91,12 +91,12 @@ func TestNewMultipartUpload(t *testing.T) {
 }
 
 func TestNewPutObjectPart(t *testing.T) {
-	abuf := bytes.Repeat([]byte("aaaaa"), 1024*1024)//至少要5MB 才能使用分片上传(不包含最后一片)
+	abuf := bytes.Repeat([]byte("aaaaa"), 1024*1024) //至少要5MB 才能使用分片上传(不包含最后一片)
 	fmt.Println(len(abuf))
 	bbuf := bytes.Repeat([]byte("b"), 1024*1024)
 	daya := minio.Core{Client: minioClient}
-	bucket:="test"
-	name :="3.log"
+	bucket := "test"
+	name := "3.log"
 	v1, err := daya.NewMultipartUpload(bucket, name, minio.PutObjectOptions{})
 	if err != nil {
 		panic(err)
@@ -105,18 +105,19 @@ func TestNewPutObjectPart(t *testing.T) {
 	var completeParts []minio.CompletePart
 	part, err := daya.PutObjectPart(bucket, name, v1, 1, bytes.NewReader(abuf), int64(len(abuf)), "", "", nil)
 	if err != nil {
-		panic(err)	}
-	fmt.Println("1",part)
+		panic(err)
+	}
+	fmt.Println("1", part)
 	completeParts = append(completeParts, minio.CompletePart{PartNumber: part.PartNumber, ETag: part.ETag})
 
 	part, err = daya.PutObjectPart(bucket, name, v1, 2, bytes.NewReader(bbuf), int64(len(bbuf)), "", "", nil)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("2",part.ETag)
-	fmt.Println("2",part.PartNumber)
-	fmt.Println("2",part.LastModified)
-	fmt.Println("2",part.Size)
+	fmt.Println("2", part.ETag)
+	fmt.Println("2", part.PartNumber)
+	fmt.Println("2", part.LastModified)
+	fmt.Println("2", part.Size)
 	completeParts = append(completeParts, minio.CompletePart{PartNumber: part.PartNumber, ETag: part.ETag})
 	_, err = daya.CompleteMultipartUpload(bucket, name, v1, completeParts)
 	if err != nil {
@@ -124,17 +125,43 @@ func TestNewPutObjectPart(t *testing.T) {
 	}
 }
 
-func TestConsolidatedFile(t *testing.T)  {
-	bucketName:="test"
-	var srcs =make([]minio.SourceInfo,0)
-	for i:=0;i<3;i++{
-		name:=fmt.Sprintf("sheet_%d",i)
-		bbuf := bytes.Repeat([]byte(fmt.Sprint(i+1)), 1)//至少要5M
-		index,err:=minioClient.PutObject(bucketName,name,bytes.NewReader(bbuf),int64(len(bbuf)),minio.PutObjectOptions{})
-		if err!=nil{
-			panic(err)
-		}
-		fmt.Println(index)
-		srcs=append(srcs,minio.NewSourceInfo(bucketName,fmt.Sprintf("sheet_%d",i),nil))
+func TestMinio(t *testing.T) {
+	var (
+		bucket = "bat-app"
+	)
+	minioClient, err := minio.New("192.168.2.28:9090", "672I9BRC71NVDCMASYQQ", "yl1AGr-ViO6queLv93EFlRv-iZ1icSvUVE8j6pi9", false)
+	if err != nil {
+		panic(err)
 	}
+	if err = minioClient.MakeBucket(bucket, ""); err != nil {
+		panic(err)
+	}
+	fmt.Println("创建桶成功:" + bucket)
+	//设置桶的权限
+	policy := `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"AWS":["*"]},"Action":["s3:ListBucketMultipartUploads","s3:GetBucketLocation","s3:ListBucket"],"Resource":["arn:aws:s3:::abc-test-1"]},{"Effect":"Allow","Principal":{"AWS":["*"]},"Action":["s3:ListMultipartUploadParts","s3:PutObject","s3:AbortMultipartUpload","s3:DeleteObject","s3:GetObject"],"Resource":["arn:aws:s3:::abc-test-1/*"]}]}`
+	err = minioClient.SetBucketPolicy(bucket, policy)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//判断文件是否存在
+	fmt.Println(minioClient.StatObject(bucket, "conf.txt", minio.StatObjectOptions{}))
+
+	//生成上传链接
+	expiry := time.Second * 24 * 60 * 60 // 1 day.
+	presignedURL, err := minioClient.PresignedPutObject(bucket, "PayDemo.java", expiry)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully generated presigned URL", presignedURL)
+	reqParams := make(url.Values)
+	reqParams.Set("Content-Type", "image/jpeg")
+	presignedURL, err = minioClient.Presign("PUT", bucket, "20190306121434.jpg", time.Second*24*60*60, reqParams)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully generated presigned URL \n", presignedURL)
 }
