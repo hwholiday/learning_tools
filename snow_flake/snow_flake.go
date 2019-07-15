e534317d1b1994c399057e61e5a0c177c19a0c7c
hwholiday <heweiholiday@gmail.com>
2019-07-15 14:31:19 +0800

    学习雪花算法

--- snow_flake/snow_flake.go
+++ snow_flake/snow_flake.go
@@ -8,70 +8,59 @@ import (
 )
 
 const (
-	nodeBits  uint8 = 10
-	stepBits  uint8 = 12
-	nodeMax   int64 = -1 ^ (-1 << nodeBits)
-	stepMax   int64 = -1 ^ (-1 << stepBits)
-	timeShift uint8 = nodeBits + stepBits
-	nodeShift uint8 = stepBits
+	workerBits  uint8 = 10
+	numberBits  uint8 = 12
+	workerMax   int64 = -1 ^ (-1 << workerBits)
+	numberMax   int64 = -1 ^ (-1 << numberBits)
+	timeShift   uint8 = workerBits + numberBits
+	workerShift uint8 = numberBits
+	startTime   int64 = 1525705533000 // 如果在程序跑了一段时间修改了epoch这个值 可能会导致生成相同的ID
 )
 
-// 起始时间戳 (毫秒数显示)
-var Epoch int64 = 1288834974657 // timestamp 2006-03-21:20:50:14 GMT
-
-// ID 结构
-type ID int64
-
-// 存储基础信息的 Node 结构
-type Node struct {
-	mu        sync.Mutex // 保证并发安全
+type Worker struct {
+	mu        sync.Mutex
 	timestamp int64
-	node      int64
-	step      int64
+	workerId  int64
+	number    int64
 }
 
-// 生成、返回唯一 snowflake ID
-func (n *Node) Generate() ID {
-	n.mu.Lock()         // 保证并发安全, 加锁
-	defer n.mu.Unlock() // 解锁
-	// 获取当前时间的时间戳 (毫秒数显示)
+func NewWorker(workerId int64) (*Worker, error) {
+	if workerId < 0 || workerId > workerMax {
+		return nil, errors.New("Worker ID excess of quantity")
+	}
+	// 生成一个新节点
+	return &Worker{
+		timestamp: 0,
+		workerId:  workerId,
+		number:    0,
+	}, nil
+}
+
+func (w *Worker) GetId() int64 {
+	w.mu.Lock()
+	defer w.mu.Unlock()
 	now := time.Now().UnixNano() / 1e6
-	if n.timestamp == now {
-		// step 步进 1
-		n.step ++
-		// 当前 step 用完
-		if n.step > stepMax {
-			// 等待本毫秒结束
-			for now <= n.timestamp {
+	if w.timestamp == now {
+		w.number++
+		if w.number > numberMax {
+			for now <= w.timestamp {
 				now = time.Now().UnixNano() / 1e6
 			}
 		}
 	} else {
-		// 本毫秒内 step 用完
-		n.step = 0
+		w.number = 0
+		w.timestamp = now
 	}
-	n.timestamp = now
-	result := ID((now-Epoch)<<timeShift | (n.node << nodeShift) | (n.step))
-	return result
-}
-// 生成一个新的 Node 类型变量
-func NewNode(node int64) (*Node, error) {
-	if node < 0 || node > nodeMax {
-		return nil, errors.New("Node number must be between 0 and 1023")
-	}
-	return &Node{
-		timestamp: 0,
-		node:      node,
-		step:      0,
-	}, nil
+	ID := int64((now-startTime)<<timeShift | (w.workerId << workerShift) | (w.number))
+	return ID
 }
 func main() {
 	// 生成节点实例
-	node, err := NewNode(1)
-	if err!=nil{
+	node, err := NewWorker(1)
+	if err != nil {
 		panic(err)
 	}
-	for  {
-		fmt.Println(node.Generate())
+	for {
+		fmt.Println(node.GetId())
 	}
 }
