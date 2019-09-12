6e9559180f3d967a982ca5afb6cabfbb3909a575
hwholiday <heweiholiday@gmail.com>
2019-09-12 11:06:39 +0800

    利用heap创建一个优先级队列

---
+++ container/heap/heap.go
@@ -0,0 +1,39 @@
+package heap
+
+type Item struct {
+	data  interface{}
+	ref   int //优先级
+	index int //在堆里面的索引
+}
+type Queue []*Item
+
+func (m Queue) Len() int {
+	return len(m)
+}
+
+func (m Queue) Less(i, j int) bool {
+	return m[i].ref > m[j].ref
+}
+
+func (m Queue) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+	m[j].index = j
+	m[i].index = i
+	//位置交换把对应的索引交换
+}
+
+func (m *Queue) Push(d interface{}) {
+	d.(*Item).index = len(*m)
+	*m = append(*m, d.(*Item))
+}
+
+func (m *Queue) Pop() interface{} {
+	//抛出后面的一个数据
+	l := len(*m)
+	s := (*m)[l-1]
+	s.index = -1
+	*m = (*m)[:l-1]
+	return s
+}
+
+
---
+++ container/heap/heap_test.go
@@ -0,0 +1,29 @@
+package heap
+
+import (
+	"container/heap"
+	"testing"
+)
+
+func TestHeap(t *testing.T) {
+	queue := make(Queue, 10)
+	for i := 0; i < 10; i++ {
+		item:=&Item{
+			data:  i + 1,
+			ref:   i + 1,
+			index: i,
+		}
+		queue[i] = item
+	}
+	heap.Init(&queue)
+	item := Item{
+		data: 8,
+		ref:  1,
+	}
+	heap.Push(&queue, &item)
+	heap.Fix(&queue, 2)
+	for queue.Len() > 0 {
+		item := heap.Pop(&queue).(*Item)
+		t.Log("index", item.index, "ref", item.ref, "val", item.data)
+	}
+}
