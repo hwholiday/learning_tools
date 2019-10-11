be7de9d4b7f45229e076ccbc7c5ba512507bc28a
hwholiday <heweiholiday@gmail.com>
2019-10-11 10:06:12 +0800

    修改进制相关

--- tool/binary_conversion/binary.go
+++ tool/binary_conversion/binary.go
@@ -7,7 +7,11 @@ func main() {
 	fmt.Printf("%064b\n", switchs)
 	switchs = 1<<63 | 1<<2 | 1<<3 //直接替换
 	fmt.Printf("%064b\n", switchs)
-	switchs += 1<<4 | 1<<5 | 1<<6 //修改某一些的东西
+	switchs = 1<<62 | 1<<61 | 1<<60 //修改某一些的东西
 	fmt.Printf("%064b\n", switchs)
-	fmt.Println(1 << 10 & switchs) //判断第二位是不是0
+	fmt.Println(switchs)
+	fmt.Printf("%064b\n", 1 << 62)
+	fmt.Printf("%064b\n", switchs)
+	fmt.Println(1 << 2 & 8070450532247928832) //判断第二位是不是0
+	fmt.Println(1 << 62 & 8070450532247928832) //判断第二位是不是0
 }
