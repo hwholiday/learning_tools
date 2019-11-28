fdfbfed4a6105bead876a72a75e252a980518a82
hwholiday <heweiholiday@gmail.com>
2019-11-28 09:47:23 +0800

    添加kafka集群搭建

--- kafka/kafka_test.go
+++ kafka/kafka_test.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/Shopify/sarama"
+	"log"
 	"testing"
 	"time"
 )
 
 func Test_Queue(t *testing.T) {
 	//同步
-	SyncProducer,err:=sarama.NewSyncProducer([]string{"localhost:9092"},nil)
+	SyncProducer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, nil)
 	CheckErr(err)
 	defer SyncProducer.Close()
 	SyncProducer.SendMessage(&sarama.ProducerMessage{
@@ -23,7 +25,7 @@ func Test_Producer(t *testing.T) {
 	config.Producer.Return.Successes = true
 	config.Producer.Return.Errors = true
 	//异步
-	AsyncProducer, err := sarama.NewAsyncProducer([]string{"localhost:9092"}, config)
+	AsyncProducer, err := sarama.NewAsyncProducer([]string{"localhost:9092","localhost:9093","localhost:9094"}, config)
 	CheckErr(err)
 	defer AsyncProducer.AsyncClose()
 	go func() {
@@ -38,14 +40,77 @@ func Test_Producer(t *testing.T) {
 	}()
 	for {
 		Message := &sarama.ProducerMessage{
-			Topic:     "Test_Producer",
+			Topic:     "ghost_topic",
 			Timestamp: time.Now(),
 		}
-		value := "消息测试 Test_Producer " + time.Now().Format("2006-01-02 15:04:05")
+		value := "消息测试 ghost_topic " + time.Now().Format("2006-01-02 15:04:05")
 		Message.Value = sarama.ByteEncoder(value)
 		AsyncProducer.Input() <- Message
 		time.Sleep(time.Second * 10)
 	}
+}
+func Test_ConsumerGroup(t *testing.T) {
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_2_0_0
+	consumerGroup, err := sarama.NewConsumerGroup([]string{"localhost:9092","localhost:9093","localhost:9094"}, "test_group", config)
+	CheckErr(err)
+	consumer := Consumer1{}
+	for {
+		if err := consumerGroup.Consume(context.Background(), []string{"ghost_topic"}, &consumer); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func Test_ConsumerGroup2(t *testing.T) {
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_2_0_0
+	consumerGroup, err := sarama.NewConsumerGroup([]string{"localhost:9092","localhost:9093","localhost:9094"}, "test_group_1", config)
+	CheckErr(err)
+	consumer := Consumer{}
+	for {
+		if err := consumerGroup.Consume(context.Background(), []string{"ghost_topic"}, &consumer); err != nil {
+			panic(err)
+		}
+	}
+}
+
+type Consumer struct {
+}
+
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for message := range claim.Messages() {
+		log.Printf("Consumer Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+		session.MarkMessage(message, "")
+	}
+	return nil
+}
+
+type Consumer1 struct {
+}
+
+func (consumer *Consumer1) Setup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (consumer *Consumer1) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (consumer *Consumer1) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for message := range claim.Messages() {
+		log.Printf("Consumer1 Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+		session.MarkMessage(message, "")
+	}
+	return nil
 }
 
 func Test_Consumer(t *testing.T) {
