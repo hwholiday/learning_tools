921736e18e96a7a8ce80c2d6b36cecee6b524f10
hwholiday <heweiholiday@gmail.com>
2019-12-31 16:27:21 +0800

    go-kit 基础模块学习(http)

--- go-kit/v1/main.go
+++ go-kit/v1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"learning_tools/go-kit/v1/v1_endpoint"
 	"learning_tools/go-kit/v1/v1_service"
 	"learning_tools/go-kit/v1/v1_transport"
@@ -11,5 +12,6 @@ func main() {
 	server := v1_service.NewService()
 	endpoints := v1_endpoint.NewEndPointServer(server)
 	httpHandler := v1_transport.NewHttpHandler(endpoints)
+	fmt.Println("server run 0.0.0.0:8888")
 	_ = http.ListenAndServe("0.0.0.0:8888", httpHandler)
 }
