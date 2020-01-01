3e8c95ffea404be1d5a326370527015897ac5d40
hwholiday <heweiholiday@gmail.com>
2020-01-01 22:59:30 +0800

    修改readme

--- go-kit/v2/utils/log.go
+++ go-kit/v2/utils/log.go
@@ -9,7 +9,8 @@ var logger *zap.Logger
 
 func NewLoggerServer() {
 	logger = logtool.NewLogger(
-		logtool.SetAppName("test_app"),
+		logtool.SetAppName("go-kit"),
+		logtool.SetLogFileDir("F:\\GoProject\\src\\learning_tools\\go-kit\\v2\\logs"),
 		logtool.SetDevelopment(true),
 		logtool.SetLevel(zap.DebugLevel),
 	)
