2b1a1974179a955a4d1d04b4a796847ce6b851f8
hwholiday <heweiholiday@gmail.com>
2020-03-10 13:15:45 +0800

    添加推送服务

--- go_push/gateway/http_server.go
+++ go_push/gateway/http_server.go
@@ -7,6 +7,8 @@ import (
 )
 
 func InitHttpServer() {
+	//curl  http://0.0.0.0:9999/push/all -X POST -d "val=test push&tag=3"
+	//curl  http://0.0.0.0:9999/push/room -X POST -d "val=test push&tag=3&id=1"
 	mux := http.NewServeMux()
 	mux.HandleFunc("/push/all", HttpPushAll)
 	mux.HandleFunc("/push/room", HttpPushRoom)
