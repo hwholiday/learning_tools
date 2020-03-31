7588944839dbcbcadc451627eb3e27002144e9d5
hwholiday <heweiholiday@gmail.com>
2020-03-31 10:42:37 +0800

    测试goconvey

---
+++ goconvey/goconvey.go
@@ -0,0 +1,5 @@
+package goconvey
+
+func Add(a, b int) int {
+	return a + b
+}
---
+++ goconvey/goconvey_test.go
@@ -0,0 +1,12 @@
+package goconvey
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	Convey("将两数相加", t, func(c C) {
+		c.So(Add(1, 2), ShouldEqual, 3)
+	})
+}
