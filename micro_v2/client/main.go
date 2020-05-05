42ff472ba1dc72066ec53c1e9f1d4f7d3c12d42d
hwholiday <heweiholiday@gmail.com>
2020-05-05 21:44:22 +0800

    添加 micro v2

---
+++ micro_v2/client/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/client"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
+	test_agent "micro"
+	"os"
+	"time"
+)
+
+func main() {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+	micReg := etcd.NewRegistry(registry.Addrs("127.0.0.1:2379"))
+	service := micro.NewService(micro.Name("srv.test.client"), micro.Registry(micReg))
+	service.Init()
+	agent := test_agent.NewTestService("srv.test", service.Client())
+
+	var opss client.CallOption = func(o *client.CallOptions) {
+		o.RequestTimeout = time.Second * 30
+		o.DialTimeout = time.Second * 30
+	}
+
+	info, err := agent.RpcUserInfo(context.TODO(), &test_agent.ReqMsg{
+		UserName: "test user",
+	}, opss)
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	fmt.Println(info)
+}
---
+++ micro_v2/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
+	test_agent "micro"
+	"os"
+	"time"
+)
+
+type Agent struct {
+}
+
+func (a *Agent) RpcUserInfo(ctx context.Context, in *test_agent.ReqMsg, out *test_agent.ResMsg) error {
+	out.Error = &test_agent.Error{
+		Code: 200,
+	}
+	out.Info = "test success"
+	fmt.Println("server  RpcUserInfo", "in", in, "out", out)
+	return nil
+}
+
+func main() {
+	micReg := etcd.NewRegistry(registry.Addrs("127.0.0.1:2379"))
+	service := micro.NewService(
+		micro.Name("srv.test"),
+		micro.Registry(micReg),
+		micro.RegisterTTL(time.Second*30),
+		micro.RegisterInterval(time.Second*10),
+	)
+	service.Init()
+	if err := test_agent.RegisterTestHandler(service.Server(), &Agent{}); err != nil {
+		fmt.Println("1", err.Error())
+		os.Exit(0)
+	}
+	if err := service.Run(); err != nil {
+		fmt.Println("2", err.Error())
+		os.Exit(0)
+	}
+}
