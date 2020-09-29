459b22f213e00eb116a7f0624e292c49e4010f68
hwholiday <heweiholiday@gmail.com>
2020-09-29 14:05:37 +0800

    修改错误提示

--- micro_v2/client/main.go
+++ micro_v2/client/main.go
@@ -6,10 +6,11 @@ import (
 	"github.com/micro/go-micro/v2/client"
 	"github.com/micro/go-micro/v2/client/grpc"
 	"github.com/micro/go-micro/v2/client/selector"
+	"github.com/micro/go-micro/v2/errors"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/registry/etcd"
+	"log"
 	test_agent "micro_v2"
-	"os"
 	"time"
 )
 
@@ -22,20 +23,24 @@ func main() {
 			selector.Registry(micReg),
 		)),
 	))
-
 	var opss client.CallOption = func(o *client.CallOptions) {
 		o.RequestTimeout = time.Second * 30
 		o.DialTimeout = time.Second * 30
 		o.Retries = 3
-		o.Address = []string{"127.0.0.1:8081"}
+		o.Address = []string{"127.0.0.1:8080"}
 	}
 	info, err := agent.RpcUserInfo(context.TODO(), &test_agent.ReqMsg{
 		UserName: "test user",
 	}, opss)
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
+		s := errors.FromError(err)
+		if s != nil { // 可转为Status
+			log.Println(s.GetCode())
+			log.Println(s.String())
+		} else { // 普通error
+			log.Println(err)
+		}
 	}
 	fmt.Println(info)
 }
--- micro_v2/go.mod
+++ micro_v2/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/golang/protobuf v1.4.2
 	github.com/micro/go-micro/v2 v2.8.0
+	google.golang.org/grpc v1.26.0
 )
