63d30c5b0bb16d9eb090f6aa4b3347ef7f983796
hwholiday <heweiholiday@gmail.com>
2020-10-16 10:39:09 +0800

    添加位运算

--- tool/binary_conversion/binary.go
+++ tool/binary_conversion/binary.go
@@ -7,11 +7,14 @@ func main() {
 	fmt.Printf("%064b\n", switchs)
 	switchs = 1<<63 | 1<<2 | 1<<3 //直接替换
 	fmt.Printf("%064b\n", switchs)
+	switchs = 1 << 0
+	fmt.Printf("%064b\n", switchs)
 	switchs = 1<<62 | 1<<61 | 1<<60 //修改某一些的东西
 	fmt.Printf("%064b\n", switchs)
 	fmt.Println(switchs)
-	fmt.Printf("%064b\n", 1 << 62)
+	fmt.Printf("%064b\n", 1<<62)
 	fmt.Printf("%064b\n", 8070450532247928832)
-	fmt.Println(1 << 2 & 8070450532247928832) //判断第二位是不是0
+	fmt.Println(1 << 2 & 8070450532247928832)  //判断第二位是不是0
 	fmt.Println(1 << 62 & 8070450532247928832) //判断第二位是不是0
+	fmt.Println(1 << 1 & 8070450532247928832)  //判断第二位是不是0
 }
