e99fc1b3336c06cd67cfb239cee2b44be73fe7df
hwholiday <heweiholiday@gmail.com>
2021-01-28 17:59:23 +0800

    完善istio 部署文档

--- istio/gateway/build.sh
+++ istio/gateway/build.sh
@@ -1,3 +1,3 @@
 CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o app .
-sudo docker image build -t hwholiday/gateway:v3.01 .
-sudo docker push hwholiday/gateway:v3.01
+sudo docker image build -t hwholiday/gateway:v3.0.3 .
+sudo docker push hwholiday/gateway:v3.0.3
