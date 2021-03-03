b24fddbe885dfe713d4369c7d7e6317c8fb828a6
hwholiday <heweiholiday@gmail.com>
2021-03-03 14:49:27 +0800

    添加 go.mod 添加基于etcd的服务发现

--- gin/model/db.go
+++ gin/model/db.go
@@ -1,47 +1,45 @@
 package model
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/cihub/seelog"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
-	"github.com/cihub/seelog"
-	"os"
-	"fmt"
 	"io/ioutil"
-	"encoding/json"
-	"github.com/go-xorm/core"
+	"os"
 )
 
 type dbInfo struct {
-	User string `json:"mysqluser"`
-	Pass string `json:"mysqlpass"`
-	Url  string `json:"mysqlurl"`
-	Port  string `json:"mysqlport"`
-	DbName   string `json:"mysqldb"`
+	User   string `json:"mysqluser"`
+	Pass   string `json:"mysqlpass"`
+	Url    string `json:"mysqlurl"`
+	Port   string `json:"mysqlport"`
+	DbName string `json:"mysqldb"`
 }
+
 var db *xorm.Engine
 
 func InitDb(path string) {
 	var err error
-	info:=getDbInfoByPath(path)
+	info := getDbInfoByPath(path)
 	checkErr(err)
-	sql:=fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8",info.User, info.Pass,info.Url,info.Port,info.DbName)
+	sql := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8", info.User, info.Pass, info.Url, info.Port, info.DbName)
 	fmt.Println(sql)
 	db, err = xorm.NewEngine("mysql", sql)
 	checkErr(err)
 	checkErr(db.Ping())
-	tbMapper := core.NewPrefixMapper(core.SnakeMapper{}, "control_")
-	db.SetTableMapper(tbMapper)
 	db.ShowSQL(true)
 }
-func getDbInfoByPath(path string)(db dbInfo){
-	data,err:=ioutil.ReadFile(path)
+func getDbInfoByPath(path string) (db dbInfo) {
+	data, err := ioutil.ReadFile(path)
 	checkErr(err)
-	err=json.Unmarshal(data,&db)
+	err = json.Unmarshal(data, &db)
 	checkErr(err)
 	return
 }
 
-func checkErr(err error){
+func checkErr(err error) {
 	if err != nil {
 		seelog.Error(err)
 		seelog.Flush()
