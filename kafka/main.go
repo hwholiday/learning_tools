5f4c98dac955c74a80395450245d7d303051c5cd
hwholiday <heweiholiday@gmail.com>
2021-06-03 17:01:20 +0800

    ebcdic to string

--- kafka/main.go
+++ kafka/main.go
@@ -24,6 +24,7 @@ func SaramaProducer() {
 	//是否等待成功和失败后的响应,只有上面的RequireAcks设置不是NoReponse这里才有用.
 	config.Producer.Return.Successes = true
 	config.Producer.Return.Errors = true
+	config.Producer.Retry.Max = 1000
 	//设置使用的kafka版本,如果低于V0_10_0_0版本,消息中的timestrap没有作用.需要消费和生产同时配置
 	//注意，版本设置不对的话，kafka会返回很奇怪的错误，并且无法成功发送消息
 	config.Version = sarama.V2_2_0_0
