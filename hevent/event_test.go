a276e49964ac045663280a652965b4ca5419e6d6
hwholiday <heweiholiday@gmail.com>
2021-12-06 16:46:47 +0800

    基于channel的简单事件通知

--- hevent/event_test.go
+++ hevent/event_test.go
@@ -7,16 +7,23 @@ import (
 )
 
 func TestName(t *testing.T) {
-	var ch = make(chan HEvent, 10)
+	var ch = make(chan HEvent)
+	var ch2 = make(chan HEvent)
 	HEventSrv().Sub("hw", ch)
+	HEventSrv().Sub("hw", ch2)
 	//go GetHEventSrv().Push("topic1", "Hi topic 1")
 	go func() {
 		for {
-			time.Sleep(1000 * time.Second)
+			time.Sleep(1 * time.Second)
 			HEventSrv().Push("hw", "Hi topic 1")
 		}
 	}()
 	for {
-		fmt.Println(<-ch)
+		select {
+		case c := <-ch:
+			fmt.Println("ch", c)
+		case c := <-ch2:
+			fmt.Println("ch2", c)
+		}
 	}
 }
