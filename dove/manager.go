899c3cdffdbd53a21da37bc7d7a0eb5b76939912
hwholiday <heweiholiday@gmail.com>
2022-10-07 21:55:17 +0800

    修改目录结构

--- dove/manager.go
+++ dove/manager.go
@@ -2,9 +2,10 @@ package dove
 
 import (
 	"errors"
-	"github.com/hwholiday/ghost/dove/network"
 	"sync"
 	"sync/atomic"
+
+	"github.com/hwholiday/learning_tools/dove/network"
 )
 
 var ErrExceedsLengthLimit = errors.New("exceeds length limit")
